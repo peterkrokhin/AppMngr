7b727a680ccbf0a9ca91fc18d4f71d4a26db2d8d
peterkrokhin <p.krokhin@gmail.com>
2021-02-08 10:59:34 +0500

    Рефакторинг
    - Реализован контроллер DateFieldsController.

---
+++ src/AppMngr.Web/Controllers/DateFieldsController copy.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using AppMngr.Application;
+
+namespace AppMngr.Web
+{
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Route("api/[controller]")]
+    public class DateFieldsController : ControllerBase
+    {
+        private IMediator _mediator; 
+
+        public DateFieldsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>Вернуть поле с типом дата по id (admin)</summary>
+        // [Authorize(Roles="admin")]
+        [HttpGet("{dateFieldId}")]
+        public async Task<ActionResult<IEnumerable<DateFieldDto>>> GetDateField(int dateFieldId)
+        {
+            var query = new GetDateFieldByIdQuery(dateFieldId);
+            return Ok(await _mediator.Send(query));
+        }
+
+        /// <summary>Добавить новое поле с типом дата (admin)</summary>
+        // [Authorize(Roles="admin")]
+        [HttpPost]
+        public async Task<ActionResult<DateFieldDto>> CreateApp(CreateDateFieldCommand command)
+        {
+            var dateField = await _mediator.Send(command);
+
+            return CreatedAtAction(
+                nameof(GetDateField),
+                new {dateFieldId = dateField.Id},
+                dateField);
+        }
+
+        /// <summary>Измененить поле с типом дата(admin)</summary>
+        // [Authorize(Roles="admin")]
+        [HttpPatch("{dateFieldId:int:min(1)}")]
+        public async Task<ActionResult> PatchDateField(int dateFieldId, UpdateDateFieldCommand command)
+        {
+            command.Id = dateFieldId;
+
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
